811a3581b6974a0210111a2c942c65fe4d0ca02d
MatthewKatzen <matthewkatzen@gmail.com>
2019-06-27 19:06:42 +1000

    generalise RHS and EQS

--- Clean.R
+++ Clean.R
@@ -5,14 +5,15 @@ setwd("C:/Users/Matthew/Google Drive/Uni/19/Thesis/Analysis/Dissorderly Bidding"
 library(tidyverse)
 library(openxlsx)
 
-#Download RHS and MV
-url <- "http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/2019/MMSDM_2019_04/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DISPATCHCONSTRAINT_201904010000.zip"
+### Download RHS and MV
+yearmonth <- "201905" #change this to the date the constraint became effective
+year <- substr(yearmonth, 1, 4)
+month <- substr(yearmonth, 5, 6)
+url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", year,"/MMSDM_", year, "_", month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.zip")
 location <- paste0(getwd(),"/data")
 temp <- tempfile()
 download.file(url, temp, mode="wb")
-unzip(temp, "PUBLIC_DVD_DISPATCHCONSTRAINT_201904010000.CSV", exdir = location)
-#rhs.2 <- read.csv("data/rhs201904.csv", skip = 1) #old version which took spreadhseet but only the first 1048574 lines
-
+unzip(temp, paste0("PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"), exdir = location)
 
 #Clean RHS and MV
 rhs <- read.csv("data/PUBLIC_DVD_DISPATCHCONSTRAINT_201904010000.CSV", sep=",",skip=1)
@@ -22,15 +23,15 @@ write.csv(rhs, "data/rhs.csv")
 unlink("data/PUBLIC_DVD_DISPATCHCONSTRAINT_201904010000.CSV")#delete big csv
 
 
-#Constraint Equations (LHS) (make sure in long format)
-lhs <- read.csv("data/lhs201904.csv", skip = 1)
+### Download Constraint Equations (RHS) e.g. S>V_NIL_SETX_SETX1
+effective.ym <- "201905" #change this to the date the constraint became effective
+e.year <- substr(effective.ym, 1, 4)
+e.month <- substr(effective.ym, 5, 6)
+e.url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", e.year,"/MMSDM_", e.year, "_", e.month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.zip")
+location <- paste0(getwd(),"/data")
+temp <- tempfile()
+download.file(e.url, temp, mode="wb")
+unzip(temp, paste0("PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.CSV"), exdir = location)
 
-lhs <- lhs %>% select(GENCONID, EFFECTIVEDATE, VERSIONNO, SCOPE, TERMID, SPD_ID, SPD_TYPE, FACTOR, LASTCHANGED)
-
-temp1 <- lhs %>% filter(GENCONID == "S>SETXH1_SETXL2") %>% filter(VERSIONNO==1)
-temp2 <- lhs %>% filter(GENCONID == "S>SETXH1_SETXL2") %>% filter(VERSIONNO==2)
-temp1==temp2
-temp1[2,]
-temp2[2,]
-
-#Generator Bids
+#clean
+eqs <- read.csv(paste0("data/PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective, "010000.CSV"), sep=",",skip=1)
