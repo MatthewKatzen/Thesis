59a44f703a4d4ca5ffd6ddafdeb6ac89ffcc6081
MatthewKatzen <matthewkatzen@gmail.com>
2019-06-28 22:56:50 +1000

    begin analysis of "T>T_LIPM_110_2A"

--- Explore.R
+++ Explore.R
@@ -1,6 +1,11 @@
-###Run Clean.R to get df
-setwd("C:/Users/Matthew/Google Drive/Uni/19/Thesis/Analysis")
+###Run Clean.R to get dfs
+setwd("C:/Users/Matthew/Google Drive/Uni/19/Thesis/Analysis/Dissordely Bidding")
 #install.packages('tidyverse')
 library(tidyverse)
-rhs <- read.csv("data/rhs.csv") %>% 
-    select(SETTLEMENTDATE, CONSTRAINTID, RHS, MARGINALVALUE) 
+
+constraint <- "T>T_LIPM_110_2A"
+rhs <- read.csv("data/rhs.csv") 
+temp <- rhs %>% filter(CONSTRAINTID == "T>T_LIPM_110_2A")
+head(temp)
+lhs <- EQS.fun("T>T_LIPM_110_2A", 201904)
+
