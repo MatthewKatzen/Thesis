f88622f9b5303a6d9e70be1fd2d4ca6d943462cb
MatthewKatzen <matthewkatzen@gmail.com>
2019-06-28 22:56:36 +1000

    convert eqs to function

--- Clean.R
+++ Clean.R
@@ -16,39 +16,39 @@ download.file(url, temp, mode="wb")
 unzip(temp, paste0("PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"), exdir = location)
 
 #Clean RHS and MV
-rhs <- read.csv("data/PUBLIC_DVD_DISPATCHCONSTRAINT_201904010000.CSV", sep=",",skip=1)
+rhs <- read.csv(paste0("data/PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"), sep=",",skip=1)
 rhs <- rhs %>% filter(MARGINALVALUE != 0) %>% #remove unconstrained
     filter(substr(CONSTRAINTID,1,1) %in% c('Q','N','V','S','T','I')) #no fcas and other weird types
 write.csv(rhs, "data/rhs.csv")
-unlink("data/PUBLIC_DVD_DISPATCHCONSTRAINT_201904010000.CSV")#delete big csv
+unlink(paste0("data\\PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"))#delete big csv
 
 
 ### Download Constraint Equations (RHS) e.g. S>V_NIL_SETX_SETX1
-effective.ym <- "201905" #change this to the date the constraint became effective
-e.year <- substr(effective.ym, 1, 4)
-e.month <- substr(effective.ym, 5, 6)
-e.url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", e.year,"/MMSDM_", e.year, "_", e.month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.zip")
-location <- paste0(getwd(),"/data")
-temp <- tempfile()
-download.file(e.url, temp, mode="wb")
-unzip(temp, paste0("PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.CSV"), exdir = location)
+#for now it is a function which takes the constraint name and yearmonth it was made as inputs and ouputs
+#the lhs form (Scale, and T)
+#In the future should probably create entire database
 
-#Clean EQS
-eqs <- read.csv(paste0("data/PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.CSV"), sep=",",skip=1)
-constraint <- "S>V_NIL_SETX_SETX1" #change this to constraint name
-temp <- eqs %>% select(GENCONID, EFFECTIVEDATE, SCOPE, SPD_ID, SPD_TYPE, FACTOR, LASTCHANGED) %>% 
-    filter(SCOPE == "DS") %>% #only care about dipatch constraints 
-    filter(GENCONID == constraint) #get constraint we care about
-
-#extract only EQS for latest LATCHANGED
-t <- unique(temp$LASTCHANGED)
-if (length(t)>1){
-    temp <- temp %>% filter(LASTCHANGED == t[length(t)])
+EQS.fun <- function(constraint, effective.ym) {
+    effective.ym <- "201904" #change this to the date the constraint became effective
+    e.year <- substr(effective.ym, 1, 4)
+    e.month <- substr(effective.ym, 5, 6)
+    e.url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", e.year,"/MMSDM_", e.year, "_", e.month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.zip")
+    location <- paste0(getwd(),"/data")
+    temp <- tempfile()
+    download.file(e.url, temp, mode="wb")
+    unzip(temp, paste0("PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.CSV"), exdir = location)
+    
+    #Clean EQS
+    eqs <- read.csv(paste0("data/PUBLIC_DVD_GENERICCONSTRAINTRHS_", effective.ym, "010000.CSV"), sep=",",skip=1)
+    temp <- eqs %>% select(GENCONID, EFFECTIVEDATE, SCOPE, SPD_ID, SPD_TYPE, FACTOR) %>% 
+        filter(SCOPE == "DS") %>% #only care about dipatch constraints 
+        filter(GENCONID == constraint) #get constraint we care about
+    
+    lhs <- temp %>% distinct() %>% #remove duplicate rows
+        filter(SPD_TYPE %in% c("T","I") | SPD_ID == "Scale") #only get scale value, interconnector, and generator/load data
+    write.csv(lhs, "data/lhs.csv")
+    unlink(paste0("data/PUBLIC_DVD_GENERICCONSTRAINTRHS_", yearmonth, "010000.CSV"))#delete big csv
+    return(lhs)
 }
-
-
-lhs <- temp %>% filter(SPD_TYPE %in% c("T","I") | SPD_ID == "Scale")
-
-
 
 ### Download Bids
