08dec73c1ac979ce89886131cfe8bf5e9ff8684a
MatthewKatzen <matthewkatzen@gmail.com>
2019-07-04 19:10:13 +1000

    RHS as function, fix bids

--- Clean.R
+++ Clean.R
@@ -5,24 +5,30 @@ setwd("C:/Users/Matthew/Google Drive/Uni/19/Thesis/Analysis/Dissorderly Bidding"
 library(tidyverse)
 library(openxlsx)
 library(sqldf)
+setwd("C:/Users/Matthew/Google Drive/Uni/19/Thesis/Analysis/Dissordely Bidding")
+external.data.location <- "D:/Thesis/Data" #for big data
 
 ### RHS and MV
-yearmonth <- "201905" #change this to the date the constraint became effective
-year <- substr(yearmonth, 1, 4)
-month <- substr(yearmonth, 5, 6)
-url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", year,"/MMSDM_", year, "_", month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.zip")
-location <- paste0(getwd(),"/data")
-temp <- tempfile()
-download.file(url, temp, mode="wb")
-unzip(temp, paste0("PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"), exdir = location)
-
-#Clean RHS and MV
-rhs <- read.csv(paste0("data/PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"), sep=",",skip=1)
-rhs <- rhs %>% filter(MARGINALVALUE != 0) %>% #remove unconstrained
-    filter(substr(CONSTRAINTID,1,1) %in% c('Q','N','V','S','T','I')) #no fcas and other weird types
-write.csv(rhs, "data/rhs.csv")
-unlink(paste0("data\\PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"))#delete big csv
-
+RHS.fun <- function(yearmonth){
+    yearmonth <- "201905" #change this to the date the constraint became effective
+    year <- substr(yearmonth, 1, 4)
+    month <- substr(yearmonth, 5, 6)
+    url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", year,"/MMSDM_", year, "_",
+                  month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth,
+                  "010000.zip")
+    temp <- tempfile()
+    download.file(url, temp, mode="wb") #download zip
+    unzip(temp, paste0("PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"), 
+          exdir = external.data.location) #unzip[ zipped file and save csv to external storage
+    
+    #Clean RHS and MV
+    rhs <- read.csv(paste0(external.data.location,"/PUBLIC_DVD_DISPATCHCONSTRAINT_", yearmonth, "010000.CSV"),
+                    sep=",",skip=1)
+    rhs <- rhs %>% filter(MARGINALVALUE != 0) %>% #remove unconstrained
+        filter(substr(CONSTRAINTID,1,1) %in% c('Q','N','V','S','T','I')) #no fcas and other weird types
+    unlink(temp) #delete zip
+    return(rhs, "data/rhs.csv")#save locally
+}
 
 ### Download Constraint Equations (RHS) e.g. S>V_NIL_SETX_SETX1
 #for now it is a function which takes the constraint name and yearmonth it was made as inputs and outputs
@@ -72,6 +78,8 @@ temp <- bands %>% filter(BIDTYPE== "ENERGY") %>%
 
 
 ###BIDS
+#for now it just takes th elast bid file for each day. 
+#Note that the file can be altered throughout the day.
 yearmonth <- "201905" #change this to the date the constraint became effective
 year <- substr(yearmonth, 1, 4)
 month <- substr(yearmonth, 5, 6)
@@ -80,24 +88,29 @@ location <- paste0(getwd(),"/data")
 temp <- tempfile()
 download.file(url, temp, mode="wb")
 unzip(temp, paste0("PUBLIC_DVD_BIDPEROFFER_", yearmonth, "010000.CSV"), exdir = location)
-
 bids <- read.csv(paste0("data/PUBLIC_DVD_BIDPEROFFER_", yearmonth, "010000.CSV"), sep=",",skip=1)
 
-temp <- bids %>% filter(DUID == "GORDON", BIDTYPE== "ENERGY") %>% 
-    select(DUID, SETTLEMENTDATE, OFFERDATE, VERSIONNO, BANDAVAIL1, BANDAVAIL2, BANDAVAIL3, BANDAVAIL4, BANDAVAIL5, BANDAVAIL6, BANDAVAIL7, BANDAVAIL8, BANDAVAIL9, BANDAVAIL10)
+temp <- bids %>% filter(DUID == "GORDON", BIDTYPE== "ENERGY") %>%
+    filter(SETTLEMENTDATE == "2019/05/10 00:00:00") %>% #only look at 5/10 (US dates)
+    filter(VERSIONNO == max((VERSIONNO))) %>% #get last version of bid file used
+    select(DUID, SETTLEMENTDATE, OFFERDATE, VERSIONNO, PERIODID, BANDAVAIL1, BANDAVAIL2, BANDAVAIL3, BANDAVAIL4, BANDAVAIL5, BANDAVAIL6, BANDAVAIL7, BANDAVAIL8, BANDAVAIL9, BANDAVAIL10)
 
-###MATCH ACTUAL BIDS AND BANDS 
-yearmonth <- "201905" #change this to the date the constraint became effective
-year <- substr(yearmonth, 1, 4)
-month <- substr(yearmonth, 5, 6)
-url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", year,"/MMSDM_", year, "_", month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DISPATCHOFFERTRK_", yearmonth, "010000.zip")
-location <- paste0(getwd(),"/data")
-temp <- tempfile()
-download.file(url, temp, mode="wb")
-unzip(temp, paste0("PUBLIC_DVD_DISPATCHOFFERTRK_", yearmonth, "010000.CSV"), exdir = location)
+    
+###MATCH ACTUAL BIDS USED 
+#This is used for matchign what BID files are used throughout the day
+#Not used yet
 
-match <- read.csv(paste0("data/PUBLIC_DVD_DISPATCHOFFERTRK_", yearmonth, "010000.CSV"), sep=",",skip=1)
-match %>% filter(DUID == "GORDON", BIDTYPE== "ENERGY") %>% head()
+# yearmonth <- "201905" #change this to the date the constraint became effective
+# year <- substr(yearmonth, 1, 4)
+# month <- substr(yearmonth, 5, 6)
+# url <- paste0("http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/", year,"/MMSDM_", year, "_", month, "/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DISPATCHOFFERTRK_", yearmonth, "010000.zip")
+# location <- paste0(getwd(),"/data")
+# temp <- tempfile()
+# download.file(url, temp, mode="wb")
+# unzip(temp, paste0("PUBLIC_DVD_DISPATCHOFFERTRK_", yearmonth, "010000.CSV"), exdir = location)
+# 
+# match <- read.csv(paste0("data/PUBLIC_DVD_DISPATCHOFFERTRK_", yearmonth, "010000.CSV"), sep=",",skip=1)
+# match %>% filter(DUID == "GORDON", BIDTYPE== "ENERGY") 
 
 
 ###DISPACTCH
@@ -111,4 +124,5 @@ download.file(url, temp, mode="wb")
 unzip(temp, paste0("PUBLIC_DVD_DISPATCH_UNIT_SCADA_", yearmonth, "010000.CSV"), exdir = location)
 
 dispatch <- read.csv(paste0("data/PUBLIC_DVD_DISPATCH_UNIT_SCADA_", yearmonth, "010000.CSV"), sep=",",skip=1)
-temp <- dispatch %>% filter(DUID == "GORDON")
+temp <- dispatch %>% filter(DUID == "GORDON") %>% 
+    filter(as.Date(SETTLEMENTDATE) == "2019-05-10")
