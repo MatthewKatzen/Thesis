4de3216ba35f6ed7a5fa8a85ed68a361bff81f9e
MatthewKatzen <matthewkatzen@gmail.com>
2019-08-10 19:01:01 +1000

    additional data for mpa

--- MPA.R
+++ MPA.R
@@ -5,3 +5,21 @@ dates <- seq(ymd(from), ymd(to), by = "day") %>% #list of dates
 
 mpa.current <- dates %>% map(~ mpa.fun(.x)) %>% 
     bind_rows()
+
+#stopped
+external.data.location <- "D:/Thesis/Data/MPA" #for big data
+files <- paste0(external.data.location, "/", list.files(external.data.location))
+mpa.current <- files %>% map(~ mpa.clean.fun(.x)) %>% 
+    bind_rows()
+
+write.csv(mpa.current, "data/mpa.current.csv")
+mpa.current <- read.csv("data/mpa.current.csv")
+
+#get gen fuel types
+fuel <- read.csv("data/dontupload/GenFuelTypes.csv")
+fuel <- fuel %>% select(DUID, Region, Classification, 7:10)
+head(fuel)
+
+#get rrp
+rrp <- rrp.fun("201807")
+head(rrp)
